27770a4cda1d835a052e02f896d8bed6feeabd03
Misha Potapenko <potapenko05121985@gmail.com>
2020-03-18 22:39:42 +0200

    first commit

---
+++ WinFormProject/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WinFormProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.forName = new System.Windows.Forms.TextBox();
+            this.forSurname = new System.Windows.Forms.TextBox();
+            this.Division = new System.Windows.Forms.Label();
+            this.forDivision = new System.Windows.Forms.TextBox();
+            this.Gender = new System.Windows.Forms.Label();
+            this.Age = new System.Windows.Forms.Label();
+            this.forAge = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.Label();
+            this.forGender = new System.Windows.Forms.ComboBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.DataField = new System.Windows.Forms.DataGridView();
+            this.Search = new System.Windows.Forms.Label();
+            this.forSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(255, 9);
+            this.Title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(163, 32);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "EMPLOYEE";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(133, 49);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(43, 20);
+            this.name.TabIndex = 1;
+            this.name.Text = "Name";
+            // 
+            // forName
+            // 
+            this.forName.Location = new System.Drawing.Point(208, 44);
+            this.forName.Name = "forName";
+            this.forName.Size = new System.Drawing.Size(256, 25);
+            this.forName.TabIndex = 0;
+            // 
+            // forSurname
+            // 
+            this.forSurname.Location = new System.Drawing.Point(208, 83);
+            this.forSurname.Name = "forSurname";
+            this.forSurname.Size = new System.Drawing.Size(256, 25);
+            this.forSurname.TabIndex = 1;
+            // 
+            // Division
+            // 
+            this.Division.AutoSize = true;
+            this.Division.Location = new System.Drawing.Point(133, 219);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(60, 20);
+            this.Division.TabIndex = 3;
+            this.Division.Text = "Division";
+            // 
+            // forDivision
+            // 
+            this.forDivision.Location = new System.Drawing.Point(208, 214);
+            this.forDivision.Name = "forDivision";
+            this.forDivision.Size = new System.Drawing.Size(256, 25);
+            this.forDivision.TabIndex = 4;
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Location = new System.Drawing.Point(133, 171);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(54, 20);
+            this.Gender.TabIndex = 5;
+            this.Gender.Text = "Gender";
+            // 
+            // Age
+            // 
+            this.Age.AutoSize = true;
+            this.Age.Location = new System.Drawing.Point(133, 124);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(33, 20);
+            this.Age.TabIndex = 7;
+            this.Age.Text = "Age";
+            // 
+            // forAge
+            // 
+            this.forAge.Location = new System.Drawing.Point(208, 119);
+            this.forAge.Name = "forAge";
+            this.forAge.Size = new System.Drawing.Size(256, 25);
+            this.forAge.TabIndex = 2;
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Location = new System.Drawing.Point(133, 88);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(63, 20);
+            this.Surname.TabIndex = 9;
+            this.Surname.Text = "Surname";
+            // 
+            // forGender
+            // 
+            this.forGender.FormattingEnabled = true;
+            this.forGender.Items.AddRange(new object[] {
+            "Male",
+            "Famale"});
+            this.forGender.Location = new System.Drawing.Point(208, 163);
+            this.forGender.Name = "forGender";
+            this.forGender.Size = new System.Drawing.Size(256, 28);
+            this.forGender.TabIndex = 3;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(137, 311);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(71, 33);
+            this.Insert.TabIndex = 5;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(225, 311);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(64, 33);
+            this.Update.TabIndex = 6;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(312, 311);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(64, 33);
+            this.Delete.TabIndex = 7;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(400, 311);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(66, 33);
+            this.Reset.TabIndex = 8;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // DataField
+            // 
+            this.DataField.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataField.Location = new System.Drawing.Point(137, 378);
+            this.DataField.Name = "DataField";
+            this.DataField.Size = new System.Drawing.Size(329, 216);
+            this.DataField.TabIndex = 9;
+            this.DataField.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.DataField_MouseDoubleClick);
+            // 
+            // Search
+            // 
+            this.Search.AutoSize = true;
+            this.Search.Location = new System.Drawing.Point(133, 261);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(51, 20);
+            this.Search.TabIndex = 10;
+            this.Search.Text = "Search";
+            // 
+            // forSearch
+            // 
+            this.forSearch.Location = new System.Drawing.Point(208, 261);
+            this.forSearch.Name = "forSearch";
+            this.forSearch.Size = new System.Drawing.Size(256, 25);
+            this.forSearch.TabIndex = 11;
+            this.forSearch.TextChanged += new System.EventHandler(this.forSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 615);
+            this.Controls.Add(this.forSearch);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.DataField);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.forGender);
+            this.Controls.Add(this.forAge);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.forDivision);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.forSurname);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.forName);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.Title);
+            this.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox forName;
+        private System.Windows.Forms.TextBox forSurname;
+        private System.Windows.Forms.Label Division;
+        private System.Windows.Forms.TextBox forDivision;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.Label Age;
+        private System.Windows.Forms.TextBox forAge;
+        private System.Windows.Forms.Label Surname;
+        private System.Windows.Forms.ComboBox forGender;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.DataGridView DataField;
+        private System.Windows.Forms.Label Search;
+        private System.Windows.Forms.TextBox forSearch;
+    }
+}
+
---
+++ WinFormProject/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormProject.Models;
+
+namespace WinFormProject
+{
+    public partial class Form1 : Form
+    {
+        int id = 0;
+        Employee model = new Employee();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bindGridView();
+        }
+
+       void bindGridView()
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                DataField.DataSource = db.Employee.ToList<Employee>();
+            }
+            
+        }
+
+        void resetControls()
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                forName.Clear();
+                forSurname.Clear();
+                forAge.Clear();
+                forGender.SelectedItem = null;
+                forDivision.Clear();
+            }
+           
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                model.name = forName.Text.Trim();
+                model.surname = forSurname.Text.Trim();
+                model.age = Convert.ToInt32(forAge.Text.Trim());
+                model.gender = forGender.SelectedItem.ToString();
+                model.devision = forDivision.Text.Trim();
+                db.Employee.Add(model);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    MessageBox.Show("Data Inserted Sussesfully!!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bindGridView();
+                    resetControls();
+                }
+                else
+                {
+                    MessageBox.Show("Data Insertion failed!!!", "Failued", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+         
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            resetControls();
+        }
+
+
+        private void DataField_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                id = Convert.ToInt32(DataField.SelectedRows[0].Cells[0].Value);
+                model = db.Employee.Where(x => x.id == id).FirstOrDefault();
+                forName.Text = model.name;
+                forSurname.Text = model.surname;
+                forAge.Text = model.age.ToString();
+                forGender.SelectedItem = model.gender;
+                forDivision.Text = model.devision;
+            }
+           
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                model.id = id;
+                model.name = forName.Text.Trim();
+                model.surname = forSurname.Text.Trim();
+                model.age = Convert.ToInt32(forAge.Text.Trim());
+                model.gender = forGender.SelectedItem.ToString();
+                model.devision = forDivision.Text.Trim();
+                db.Entry(model).State = EntityState.Modified;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    MessageBox.Show("Data Updated Sussesfully!!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bindGridView();
+                    resetControls();
+                }
+                else
+                {
+                    MessageBox.Show("Data Updation failed!!!", "Failued", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }  
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                DialogResult dr = MessageBox.Show("Are you sure???", "Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (dr == DialogResult.Yes)
+                {
+                    model = db.Employee.Where(x => x.id == id).FirstOrDefault();
+                    db.Entry(model).State = EntityState.Deleted;
+                    int a = db.SaveChanges();
+                    if (a > 0)
+                    {
+                        MessageBox.Show("Data Deleted Sussesfully!!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        bindGridView();
+                        resetControls();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Deletion failed!!!", "Failued", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You have cancelled this operation");
+                }
+
+            }
+        }
+
+        private void forSearch_TextChanged(object sender, EventArgs e)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                if (forSearch.Text != string.Empty)
+                {
+                    var items = db.Employee.Where(x => x.name.Contains(forSearch.Text) || x.surname.Contains(forSearch.Text) || x.gender.Contains(forSearch.Text) || x.devision.Contains(forSearch.Text));
+                    DataField.DataSource = items.ToList<Employee>();
+                }
+                else
+                {
+                    bindGridView();
+                }
+            }
+        }
+    }
+}
